d33018f724685fe433e817343f9a7f5dc03c92de
asurazur <asurazur@gmail.com>
2023-11-22 19:59:14 +0800

    Refactor: refactor server and client initialize connection block of codes New Functions: + init_server_connection - Initialize Server Socket, Server Address, Bind Socket to Server Address, Wait for Incoming Connection + init_client_connection - Initialize Client Socket, Server Address, and Make Client Connect to the Server (if any)

--- include/connection.h
+++ include/connection.h
@@ -8,9 +8,11 @@
 #include <netdb.h>
 #include <unistd.h>
 
+void init_server_connection(struct sockaddr_in*, int*, int*);
+void init_client_connection(struct sockaddr_in*, int*);
 void exit_with_error(char*);
 void init_server(struct sockaddr_in*);
-void connect_client_to_server(int, struct sockaddr_in);
+void connect_client_to_server(int*, struct sockaddr_in*);
 int listen_and_accept_connections(int);
 
 # endif
