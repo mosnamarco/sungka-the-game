70c0e27694cbaa3261c3eb7c4aa5a4a4167def18
asurazur <asurazur@gmail.com>
2023-12-05 11:36:42 +0800

    BugFix: fix error when connecting to other computer on the LAN

--- include/connection.h
+++ include/connection.h
@@ -9,11 +9,11 @@
 #include <netdb.h>
 #include <unistd.h>
 void init_server_connection(struct sockaddr_in*, int*, int*, const int, const char*, const int);
-void init_client_connection(struct sockaddr_in*, int*, const int, const char *, const char*, const int);
+void init_client_connection(struct sockaddr_in*, int*, int, char *, char*, int);
 void exit_with_error(char*);
 void init_server(struct sockaddr_in*, const int);
-void connect_client_to_server(int*, struct sockaddr_in*);
-int listen_and_accept_connections(int);
+void connect_client_to_server(struct hostent*, struct sockaddr_in*, int, int*);
+int listen_and_accept_connections(int, int);
 void send_t(int, Sungka*);
 void recv_t(int, Sungka*);
 # endif
